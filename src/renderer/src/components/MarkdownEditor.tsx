83f72fea248f5e81d0e744dfa478b7afe3cf519e
darlegui <arleguidaniel@gmail.com>
2024-04-29 00:12:33 -0700

    Finish Adding the Editor

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -1,10 +1,17 @@
-import { MDXEditor } from '@mdxeditor/editor'
+import {
+  headingsPlugin,
+  listsPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  quotePlugin
+} from '@mdxeditor/editor'
 
 export const MarkdownEditor = () => {
   return (
     <MDXEditor
-      markdown={'# Hello'}
-      contentEditableClassName="outline-none min-h-screen max-w-none w-full text-lg px-8 py-5 text-white/90 "
+      markdown={'# Heading Title'}
+      plugins={[headingsPlugin(), listsPlugin(), quotePlugin(), markdownShortcutPlugin()]}
+      contentEditableClassName="outline-none min-h-screen max-w-none w-full text-lg px-8 py-5 text-white/90 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
     />
   )
 }
